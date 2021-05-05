0ed9bdb2ccd5be6ec9ac313789c6b13694d6447a
Nata-Am <78337126+Nata-Am@users.noreply.github.com>
2021-05-04 22:14:17 -0500

    JS date & time defined

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
 //https://fervent-agnesi-854393.netlify.app
-
-function formatDate (time){
+function formatedDate(dateElements) {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day = days[time.getDay()];
-    
+    let day = days[dateElements.getDay()];
+
     let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    let month = months[time.getMonth()];
-    let date = time.getDate();
-    let year = time.getFullYear();
-    
+    let month = months[dateElements.getMonth()];
+
+    let date = dateElements.getDate();
+    let year = dateElements.getFullYear();
+
     return `${day}, ${month} ${date} ${year}`
 }
-  
+
 let today = new Date();
-let currentDay = document.querySelector(`#date`)
-currentDay.innerHTML = formatDate(today)
-    
-function formatHour (timming) {
+let currentDate = document.querySelector("#current-date")
+currentDate.innerHTML = formatedDate(today)
+
+function formatedHour(timming) {
     let hours = timming.getHours();
     if (hours < 10) {
-      hours = `0${hours}`
+        hours = `0${hours}`
     }
     let minutes = timming.getMinutes();
     if (minutes < 10) {
-      minutes = `0${minutes}`
+        minutes = `0${minutes}`
     }
-  
+
     return `${hours}:${minutes}`
-  }
-  
-  let hour = document.querySelector("#time")
-  hour.innerHTML = formatHour(today)
 
+}
 
-
-
+let hour = document.querySelector("#current-time")
+hour.innerHTML = formatedHour(today)
 
 
 let apiKey = "8c780c003118c891cdcc809594dbc9d4"
